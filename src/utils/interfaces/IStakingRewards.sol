4890d892de69b87c20f20db86722d7dff87851f8
johnnyonline <johnnyonline.eth@gmail.com>
2024-05-10 23:11:27 +0300

    feat: initial commit

---
+++ src/utils/interfaces/IStakingRewards.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.18;
+
+interface IStakingRewards {
+    function stakeFor(address recipient, uint256 amount) external;
+
+    function withdrawFor(address recipient, uint256 amount, bool exit) external;
+}

0f742b585a8c6c69a9930390511b211e801240e8
johnnyonline <johnnyonline.eth@gmail.com>
2024-07-20 23:29:16 +0300

    fix: cleanup

--- script/DeploySiloFactory.s.sol
+++ script/DeploySiloFactory.s.sol
@@ -36,12 +36,3 @@ contract DeploySiloFactory is Script {
         vm.stopBroadcast();
     }
 }
-forge verify-contract --etherscan-api-key $KEY --watch --chain-id 42161 --compiler-version v0.8.18+commit.87f61d96 --verifier-url https://api.arbiscan.io/api 0x127A7F610cc704Be6122dfa76eb61E84C9cb0Efd src/ERC404/BaseERC404.sol:BaseERC404
-
-// address _repository, - 0x8658047e48CC09161f4152c79155Dac1d710Ff0a
-// address _silo, - 0xA8897b4552c075e884BDB8e7b704eB10DB29BF0D
-// address _share, - 0x713fc13CaAB628F116Bc34961f22a6B44aD27668
-// address _asset, - 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8
-// address _incentivesController, - 0xCC4933B0405Ae9DDFE05a54d20f56A0447c9EBcF
-// string memory _name
---constructor-args $(cast abi-encode "constructor(address,address,address,address,address,string)" 0x8658047e48CC09161f4152c79155Dac1d710Ff0a 0xA8897b4552c075e884BDB8e7b704eB10DB29BF0D 0x713fc13CaAB628F116Bc34961f22a6B44aD27668 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8 0xCC4933B0405Ae9DDFE05a54d20f56A0447c9EBcF "Silo Lender wstETH/USDC.e")
